91e16326694d7914b290d5ea942c40fd169988e5
slosh chetty <thatawesomeindianchick@gmail.com>
2023-06-13 20:33:53 +0200

    artists

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,6 @@
 class User < ApplicationRecord
   has_many :events
-  validates :name, :email, presence: true
-
+  
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,

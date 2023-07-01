1944f11aee58ab21bae997dff93d6f4a9966a7b1
daniel thackwray <dthackwray@gmail.com>
2023-07-01 17:17:15 +0200

    homepage searchbar 01

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,5 +1,11 @@
+require "pg_search"
 class Event < ApplicationRecord
   belongs_to :event_artists
   belongs_to :venue
   validates :title, :description, :start_datetime, :end_datetime, presence: true
+  include PgSearch::Model
+  pg_search_scope :search_by_title_and_description,
+                  against: %i[title description],
+                  associated_against: { venue: :name, artist: :genre},
+                  using: { tsearch: { prefix: true } }
 end

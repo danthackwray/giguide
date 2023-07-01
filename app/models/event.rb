fd510713d5fe1191137bcb80aa82f39224841541
Amanda Foster <amanda.foster@multichoice.co.za>
2023-07-01 09:25:03 +0200

    event validations and associations done

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,4 +1,4 @@
 class Event < ApplicationRecord
-  belongs_to :user
-  belongs_to :venue
+  belongs_to :event_artists, :venue
+  validates :title, :description, :start_datetime, :end_datetime, presence: true
 end

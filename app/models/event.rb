f5d3df1c5d3f7da60f52c4b9cd2aae7daf396f90
Amanda Foster <amanda.foster@multichoice.co.za>
2023-07-05 19:11:42 +0200

    cards updated

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,4 +1,5 @@
 class Event < ApplicationRecord
-  belongs_to :event_artists, :venue
+  has_many :event_artists
+  belongs_to :venue
   validates :title, :description, :start_datetime, :end_datetime, presence: true
 end

1d1dc5f861382e1e7823eb4919cdf22c834d0275
Liam Strickland <liam.strickland96@gmail.com>
2023-07-06 17:43:32 +0200

    map popups

--- app/controllers/venues_controller.rb
+++ app/controllers/venues_controller.rb
@@ -7,7 +7,8 @@ class VenuesController < ApplicationController
     @markers = @venues.geocoded.map do |venue|
       {
         lat: venue.latitude,
-        lng: venue.longitude
+        lng: venue.longitude,
+        info_window_html: render_to_string(partial: "info_window", locals: {venue: venue})
       }
     end
   end
